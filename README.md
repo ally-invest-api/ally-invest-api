2f3d8a68cdf2fa0e1d64a968bc016706a311e130
Amit Gupta <dev@ipsquare.com>
2020-05-17 15:19:01 -0700

    working

---
+++ README.md
@@ -0,0 +1,134 @@
+# ally-invest-api
+Abstraction for ally invest (tradeking) api
+
+## Credits
+Thanks to @codeNgamer, @dimfeld & anyone who worked on it for their contributions.
+
+## Install
+> npm install --save ally-invest-api
+
+## Init
+```javascript
+const allyInvestApi = require('ally-invest-api');
+
+// Setup key/secret for authentication and API endpoint URL
+const configuration = {
+  consumerKey: 'Your key',
+  consumerSecret: 'Secret',
+  oauthToken: 'Token',
+  oauthTokenSecret: 'TokenSecret',
+};
+const tkClient = new allyInvestApi(configuration);
+// tkClient.setResponseType('xml');
+```
+## Api
+___
+
+### setResponseType(type)
+Sets the default response type for every request returned. Valid types are 'xml' and 'json'.
+Note default response type is json.
+
+### getResponseType()
+returns the current default response type
+
+### accounts([accountId])
+This  will return detailed balance and holding information for each account associated with the current user if accountId is not provided. Else it will return detailed balance and holding information for the accountId passed.
+
+### accountBalances()
+This call will return summary balance information for each account associated with the current user as well as the total value for all accounts associated with the user.
+
+
+### accountOrders(accountNumber)
+This call will return the most recent orders for the account number passed.
+
+### postAccountOrder(accountId, order)
+
+This call will allow you to place an order. This requires the order data is submitted in FIXML format submitted as XML within the body.
+
+Example (see [js2xmlparser](https://github.com/michaelkourlas/node-js2xmlparser) ) for pattern building orders:
+``` javascript
+const postOrder = {
+  Order: {
+    '@': {
+    TmInForce: '0',
+        Typ: '1',
+        Side: '1',
+        Acct: '12345678',
+      },
+    Instrmt: {
+      '@': {
+        SecTyp: 'CS',
+        Sym: 'GE',
+          }
+      },
+    OrdQty: {
+      Qty: '1'
+    }
+  }
+};
+
+console.log(postOrder); // order structure
+>  <Order TmInForce="0" Typ="1" Side="1" Acct="12345678">
+>    <Instrmt SecTyp="CS" Sym="GE"/>
+>    <OrdQty Qty="1"/>
+>  </Order>
+
+const tkClient = new allyInvestApi(configuration);
+tkClient.setResponseType('xml');
+tkClient.postAccountOrder('12345678', postOrder).then(response => {
+    console.log(response);
+    }).catch(err => {
+      console.log('error');
+      console.log(err);
+      });
+```
+
+### balanceForAccount(accountId)
+This will return detailed balance information for the account id.
+
+###  historyForAccount(accountId, [range], [transactions])
+This will return account activity for the account id specified in the URI. This call supports optional date range or transaction type filters.
+
+###  holdingsForAccount(accountId)
+This will return detailed information about the holdings for the account id.
+
+###  marketClock()
+This will return the current state of the market, the time of the next state change (if the market is open), and the current server timestamp.
+
+###  marketTopLists({ listType, [exchange = 'N'] })
+This call will return a ranked list based on the list type specified. You can also pass a value for exchange to change the default New York Stock Exchange
+
+###  marketNewsSearch({ symbols, [maxhits = 10], [startdate], [enddate] })
+This will return a listing of news headlines based on the current symbol(s).Note pass a string for a single symbol or an array of string symbols.
+###  memberProfile()
+This will return general information associated with the user. More importantly it will also return all of the account numbers and account information for the user.
+
+### getTimeSalesForSymbols({ symbols = 'AAPL', startdate = '2020-04-06', enddate = '2020-05-06', [interval = '1min'] })
+This call will return time and sales quote data based on a symbol passed as a query parameter (see query parameters below).
+
+### getMarketQuotesForSymbols({ symbols, [fids] })
+This will return quotes for a symbol or list of symbols. Pass a string for a single fid or an array of fids. The fids parameter should be used when a customized list of fields is desired. By default, all applicable data fields are returned.
+
+### streamMarketQuotesForSymbols({ symbols, [fids] })
+Same as getMarketQuotesForSymbols but streams the data.
+
+### utilityStatus()
+This will return the current server timestamp if the API and its backend systems are accessible. Otherwise it will return an error.
+### watchLists([watchListId])
+This will retrieve a list of watchlists for the authenticated user if no watchListId is passed. Or will get the watchlist specified.
+### newWatchList(watchListId, symbols)
+This will create a watchlist with the specified id for the authenticated user.
+
+## ToDo's
+___
+1. Add the following helper methods for trading
+    * Buy Order.
+    * Sell Order.
+    * Sell Short (opening a short position).
+    * Buy to Cover (closing a short position).
+    * Change Order.
+    * Cancel Order.
+    * Buy to Open (opening a long option purchase).
+    * Sell to Open (opening a short option sell).
+    * Buy to Close (closing a previously opened short option).
+    * Sell to Close (closing a previously opened long option).
---
+++ index.js
@@ -0,0 +1,270 @@
+const oauth = require('oauth');
+const _ = require('lodash');
+const js2xmlparser = require("js2xmlparser");
+
+class allyinvestApi {
+  constructor(options) {
+    const isValidCredentials = this._validateCredentials(options);
+    if (!isValidCredentials) {
+      this._throwError('Invalid Credentials');
+      return false;
+    }
+
+    this.options = options;
+    this.apiEndPoint = 'https://api.tradeking.com/v1';
+    this.responseType = 'json';
+    this.tkClient = new oauth.OAuth(
+      'https://developers.tradeking.com/oauth/request_token',
+      'https://developers.tradeking.com/oauth/access_token',
+      this.options.consumerKey,
+      this.options.consumerSecret,
+      '1.0',
+      this.options.callback,
+      'HMAC-SHA1'
+    );
+  }
+
+  _validateCredentials(credentials) {
+    if (!credentials) return false;
+
+    const {
+      consumerKey,
+      consumerSecret,
+      oauthToken,
+      oauthTokenSecret,
+    } = credentials;
+
+    return consumerKey && consumerSecret && oauthToken && oauthTokenSecret;
+  }
+
+  _getApiEndPoint(endpoint, queryParam, stream = false, options = { useGet: true, postBody: null }) {
+    let url = `${this.apiEndPoint}/${endpoint}.${this.responseType}`;
+    if (queryParam) {
+      url = `${this.apiEndPoint}/${endpoint}.${this.responseType}?${queryParam}`;
+    }
+
+    if (stream) {
+      url = url.replace(/api/i, 'stream');
+    }
+
+    return new Promise((resolve, reject) => {
+      if (options.useGet) {
+        this.tkClient.get(
+          url,
+          this.options.oauthToken,
+          this.options.oauthTokenSecret,
+          (error, data, response) => {
+            if (error) reject(error);
+            if (this.responseType === 'xml') return resolve(data);
+            if (`${data}`.length === 0) reject(data);
+            let result
+            try {
+              result = resolve(JSON.parse(data))
+            } catch (err) {
+              console.log(data)
+              result = reject(err)
+            }
+            return result;
+          }
+        );
+      } else {
+        this.tkClient.post(
+          url,
+          this.options.oauthToken,
+          this.options.oauthTokenSecret,
+          options.postBody,
+          (error, data, response) => {
+            if (error) reject(error);
+            if (this.responseType === 'xml') return resolve(data);
+            return resolve(JSON.parse(data));
+          }
+        );
+      }
+    });
+  }
+
+  _validateId(id) {
+    if (!id) this._throwError('You must pass an account id');
+  }
+
+  _throwError(message) {
+    throw new Error(message);
+  }
+
+  _fieldIsArrayOrSingle({ field, fieldName }) {
+    if (_.isArray(field) && !field.length) {
+      this._throwError(`You must pass at least one element for ${fieldName}`);
+    }
+
+    if (_.isString(field) && !field) {
+      this._throwError(`You must pass a value for ${fieldName}`);
+    }
+
+    if (!_.isString(field) && !_.isArray(field)) this._throwError(`Invalid type ${typeof field} for ${fieldName}`);
+  }
+
+  _buildFxml(body) {
+    const options = { declaration: { include: false, encoding: 'UTF-8' }};
+    const fixmlRoot = {
+      '@': {
+        xmlns:"http://www.fixprotocol.org/FIXML-5-0-SP2",
+      },
+      ...body,
+    };
+    return js2xmlparser("FIXML", fixmlRoot, options);
+  }
+
+  _trimQueryStrings(strings, ...values) {
+    let queryString = '';
+    _(strings).forEach((string, key) => {
+      if (!values[key]) return false;
+      queryString += queryString + string + values[key];
+    });
+
+    return queryString;
+  }
+
+  setResponseType(type) {
+    switch (type) {
+      case 'json': this.responseType = type; break;
+      case 'xml': this.responseType = type; break;
+      default :
+        console.log('You passed an invalid response type, default will be used');
+        this.responseType = 'json';
+        break;
+    }
+  }
+
+  getResponseType() {
+    return this.responseType;
+  }
+
+  accounts(id) {
+    if (id) return this.getApiEndPoint(`accounts/${id}`);
+    return this._getApiEndPoint('accounts');
+  }
+
+  accountBalances() {
+    return this._getApiEndPoint('accounts/balances');
+  }
+
+  accountOrders(id) {
+    this._validateId(id);
+    return this._getApiEndPoint(`accounts/${id}/orders`);
+  }
+
+  postAccountOrder(id, order) {
+   this._validateId(id);
+   const postBody = this._buildFxml(order);
+   return this._getApiEndPoint(`accounts/${id}/orders`, null, null, { useGet: false, postBody });
+  }
+
+  balanceForAccount(id) {
+    this._validateId(id);
+    return this._getApiEndPoint(`accounts/${id}/balances`);
+  }
+
+  historyForAccount(id, range = 'all', transaction = 'all') {
+    this._validateId(id);
+    const validRangeTypes = [
+      'all',
+      'today',
+      'current_week',
+      'current_month',
+      'last_month'
+    ];
+    const validTransactionTypes = [
+      'all',
+      'bookkeeping',
+      'trade'
+    ];
+    if (range && !validRangeTypes.includes(range)) this._throwError('Invalid range passed');
+    if (transactions && !validTransactionTypes.includes(transaction)) this._throwError('Invalid transaction passed');
+
+    return this._getApiEndPoint(`accounts/${id}/history`, this._trimQueryStrings`range=${range}&transactions=${transactions}`);
+  }
+
+  holdingsForAccount(id) {
+    this._validateId(id);
+    return this._getApiEndPoint(`accounts/${id}/holdings`);
+  }
+
+  marketClock() {
+    return this._getApiEndPoint(`market/clock`);
+  }
+
+  marketTopLists({ listType, exchange = 'N' }) {
+    const validListTypes = [
+      'toplosers',
+      'toppctlosers',
+      'topvolume',
+      'topactive',
+      'topgainers',
+      'toppctgainers'
+    ];
+    const validExchanges = ['A','N','Q','U','V'];
+    const upperCaseExchange = exchange.toUpperCase();
+    if (!validListTypes.includes(listType)) this._throwError('Invalid listType supplied');
+    if (!validExchanges.includes(upperCaseExchange)) this._throwError('Invalid exchange supplied');
+
+    return this._getApiEndPoint(`market/toplists/${listType}`,`exchange=${upperCaseExchange}`);
+  }
+
+  marketNewsSearch({ symbols, maxhits = 10, startdate, enddate }) {
+    this._fieldIsArrayOrSingle({ field: symbols, fieldName: 'symbols' });
+    let formatedSymbols = symbols;
+    if (_.isArray(symbols)) {
+      formatedSymbols = formatedSymbols.join(',');
+    }
+    return this._getApiEndPoint(`market/news/search`, this._trimQueryStrings`symbols=${symbols}&maxhits=${maxhits}&startdate=${startdate}&enddate=${enddate}`);
+  }
+
+  memberProfile() {
+    return this._getApiEndPoint(`member/profile`);
+  }
+
+  getMarketQuotesForSymbols({ symbols, fids, stream = false }) {
+    this._fieldIsArrayOrSingle({ field: symbols, fieldName: 'symbols'});
+    if (fids) this._fieldIsArrayOrSingle({ field: fids, fieldName: 'fids'});
+    let formatedSymbols = symbols;
+    let formatedFids = fids;
+    if (_.isArray(symbols)) {
+      formatedSymbols = formatedSymbols.join(',');
+    }
+    if (_.isArray(fids)) {
+      formatedFids = formatedFids.join(',');
+    }
+    if (stream) return this._getApiEndPoint(`market/quotes`, this._trimQueryStrings`symbols=${formatedSymbols}&fids=${formatedFids}`, stream);
+    return this._getApiEndPoint(`market/ext/quotes`, this._trimQueryStrings`symbols=${formatedSymbols}&fids=${formatedFids}`, stream);
+  }
+
+  getTimeSalesForSymbols({ symbols = 'AAPL', startdate = '2020-04-06', enddate = '2020-05-06', interval = '1min' }) {
+    return this._getApiEndPoint(`market/timesales`, `symbols=${symbols}&startdate=${startdate}&enddate=${enddate}&interval=${interval}`);
+  }
+
+  streamMarketQuotesForSymbols(options) {
+    options.stream = true;
+    return this.getMarketQuotesForSymbols(options);
+  }
+
+  utilityStatus() {
+    return this._getApiEndPoint(`utility/status`);
+  }
+
+  watchLists(watchListName) {
+    if (watchListName) return this._getApiEndPoint(`watchlists/${watchListName}`);
+    return this._getApiEndPoint(`watchlists`);
+  }
+
+  newWatchList(watchListName, symbols) {
+    this._fieldIsArrayOrSingle({ field: symbols, fieldName: 'symbols'});
+    if (!watchListName) this._throwError('You must pass a watch list name');
+    let formatedSymbols = symbols;
+    if (_.isArray(symbols)) {
+      formatedSymbols = formatedSymbols.join(',');
+    }
+    return this._getApiEndPoint(`watchlists`, `id=${watchListName}&symbols=${formatedSymbols}`, null, { useGet: false, postBody: null });
+  }
+}
+
+module.exports =  allyinvestApi;
